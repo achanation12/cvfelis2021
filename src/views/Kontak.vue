142695516972c6f0fbca5e79ee2098624a239a87
felis <galihzpcchaikall@gmail.com>
2021-08-24 09:41:04 +0700

    fix maps mobile

--- src/views/Kontak.vue
+++ src/views/Kontak.vue
@@ -8,13 +8,19 @@
         <div class="bg-dark1">
             <h3 class="mt-3">Kontak</h3>
             <div class="container">
-                <iframe class="gmap_iframe" width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q= Jalan Layar Raya No.72, Kelapa Dua, Kelapa Dua KAB. TANGERANG - KELAPA DUA&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                <iframe class="gmap_iframe maps-h" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q= Jalan Layar Raya No.72, Kelapa Dua, Kelapa Dua KAB. TANGERANG - KELAPA DUA&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
                 <br>
-                <i class="fas fa-map-marker-alt mt-2"></i> Jalan Layar Raya No.72, Kelapa Dua, Kelapa Dua KAB. TANGERANG - KELAPA DUA
-                <br>
-                <i class="fas fa-phone"></i> +6285216359147
-                <br>
-                <i class="fas fa-envelope"></i> galihjunaedifelis@gmail.com
+                <div class="row">
+                    <div class="col-lg-4 mt-3">
+                        <i class="fas fa-phone" style="color:#082032;background-color:white;padding:5px; border-radius:100%;"></i> +6285216359147
+                    </div>
+                    <div class="col-lg-4 mt-3">
+                        <i class="fas fa-map-marker-alt" style="color:#082032;background-color:white;padding:5px; border-radius:100%;"></i> Jalan Layar Raya No.72, Kelapa Dua, Kelapa Dua KAB. TANGERANG - KELAPA DUA
+                    </div>
+                    <div class="col-lg-4 mt-3">
+                        <i class="fas fa-envelope" style="color:#082032;background-color:white;padding:5px; border-radius:100%;"></i> galihjunaedifelis@gmail.com
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -26,5 +32,15 @@
         padding: 30px;
         background-color: #082032;
         color: whitesmoke;
+    }
+
+    .maps-h {
+        height: 600px;
+    }
+
+    @media (max-width: 575.98px) {
+        .maps-h {
+            height: 50%;
+        }
     }
 </style>
